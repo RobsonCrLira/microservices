e67c4568a97ddb056ea7dadc0be66108ab6be0e7
Robson Cristian de Lira <robso.crlira@gmail.com>
2022-05-05 11:31:07 -0400

    feat: add ms create user

---
+++ create-user/src/index.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (_, res) => res.json({ message: 'MS Create-User' }));
+app.listen(4001, () => console.log('Aplication(MS Create-User) is running'));
