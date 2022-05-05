cfb30b270e1ed9b1ac2ea6ad51edc130d73ccaab
Robson Cristian de Lira <robso.crlira@gmail.com>
2022-05-05 15:14:54 -0400

    refactor: run prisma client

--- create-user/src/index.ts
+++ create-user/src/index.ts
@@ -3,4 +3,4 @@ import express from 'express';
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (_, res) => res.json({ message: 'MS Create-User' }));
-app.listen(4001, () => console.log('Aplication(MS Create-User) is running'));
+app.listen(4001, () => console.log('Application (MS Create-User) is running'));
